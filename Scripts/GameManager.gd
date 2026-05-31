5827693431fac4e643aebee08364e2c49e3b2aef
Caio Dourado <caiof.dourado@gmail.com>
2026-05-31 14:58:42 -0300

    Feat: alteração no game maneger e verificação de alterações de uid

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -29,6 +29,7 @@ var instantiating = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Game Startado")
 	check_connection()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
